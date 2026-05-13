cb08fb0a9f8d313f8bb83bb3bed4a420e8ec052c
ArrMannDev <arrmann632025@gmail.com>
2026-05-13 20:57:23 +0630

    add:shadcn and ui

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,5 @@
+import { Sidebar } from "@/components/ui/sidebar";
+
 export default function HomePage() {
-  return <div className="w-full h-screen bg-red-700">Hello</div>;
+  return <div className="w-full h-screen bg-red-700"></div>;
 }
